4a68f0f5214c806c8b1d8ddd301252939ed5c571
Jeremy Misavage <jmisavage@gmail.com>
2023-12-22 18:02:21 -0500

    feat: move to es module

--- index.test.js
+++ index.test.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const snap = require('./index');
+import { writeFileSync } from 'fs';
+import { snapper } from './index.js';
 
-test('takes screenshot of github', () => {
-    return snap({
+test('takes screenshot of github', async () => {
+    return snapper({
         url: 'https://github.com/',
         width: 300,
         height: 600,
@@ -10,6 +10,10 @@ test('takes screenshot of github', () => {
         format: 'jpeg',
         quality: 70,
     }).then(data => {
+        writeFileSync('screenshot.jpg', Buffer.from(data, 'base64'));
         expect(data).toBeTruthy();
+    }).catch((error) => {
+        // eslint-disable-next-line
+        console.error(error);
     });
 });
